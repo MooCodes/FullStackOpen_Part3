aec07f5ecf9b0937bdc9e12e91493507e8b75043
MooCodes <60076255+MooCodes@users.noreply.github.com>
2021-10-04 11:43:33 -0500

    Exercise 3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -45,9 +45,14 @@ app.get('/api/persons/:id', (req, res) => {
         res.json(person)
     }
     else {
-        res.status(404)
-        res.json({message: "Not Found"})
+        res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    persons = persons.filter(p => p.id !== Number(req.params.id))
+
+    res.status(204).end()
 })
 
 const PORT = 3001
