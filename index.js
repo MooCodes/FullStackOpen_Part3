9a48aaf7b9004f3060c5bea2a5728d5b32e98062
MooCodes <60076255+MooCodes@users.noreply.github.com>
2021-10-05 15:05:21 -0500

    Copied build from frontend to backend and telling express to use the static build dir

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body)) // store 'body' as 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
